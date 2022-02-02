4258dc92274e7aeaf355dd9fd4efaad1bb07e5b2
jesk <jesk.aikema@gmail.com>
2022-02-02 11:50:41 +0100

    session aangemaakt, head en header include gemaakt

---
+++ Pages/header.php
@@ -0,0 +1,10 @@
+<div class="headerlogo">
+    <a href="index.html"><img alt="logo" src="images/logo.jpg" height="90" width="100"/></a>
+</div>
+<div class="headernav">
+    <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="">Most hired cars</a></li>
+        <li><a href="login.php">Login</a></li>
+    </ul>
+</div>
