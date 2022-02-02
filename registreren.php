1a70feaac555b3b5b031fcd697c85b84759d193e
Sarah Vogelzang <sarah.vogelzang@student.nhlstenden.com>
2022-02-02 12:45:42 +0100

    added filters to register form

--- registreren.php
+++ registreren.php
@@ -1,6 +1,26 @@
 <?php
 
-//include_once("Config/config.php");
+include_once("Config/config.php");
+
+if(isset($_POST["registeren"])){
+    if(isset($_POST["wachtwoord"]) && $wachtwoord = filter_input(INPUT_POST, "wachtwoord", FILTER_SANITIZE_FULL_SPECIAL_CHARS)){
+        if(isset()$_POST["naam"]) && $naam = filter_input(INPUT_POST, "naam", FILTER_SANITIZE_FULL_SPECIAL_CHARS)){
+            if(isset()$_POST["achternaam"]) && $achternaam = filter_input(INPUT_POST, "achternaam", FILTER_SANITIZE_FULL_SPECIAL_CHARS)){
+                if(isset()$_POST["gdatum"]) && $gdatum = filter_input(INPUT_POST, "gdatum", FILTER_SANITIZE_FULL_SPECIAL_CHARS)){
+                    if(isset($_POST["adres"]) && $adres = filter_input(INPUT_POST, "adres", FILTER_SANITIZE_FULL_SPECIAL_CHARS)){
+                        if(isset($_POST["postcode"]) && $postcode = filter_input(INPUT_POST, "postcode", FILTER_SANITIZE_FULL_SPECIAL_CHARS)){
+                            if(isset($_POST["email"]) && $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_FULL_SPECIAL_CHARS)){
+                                $hash_password = password_hash($wachtwoord, PASSWORD_DEFAULT);
+                            }
+                        }
+
+                    }
+                }
+
+            }
+        }
+    }   
+}
 
 ?>
 
@@ -15,12 +35,16 @@
 <body>
 <form action="<?=htmlspecialchars($_SERVER['PHP_SELF'])?>" method="POST">
 <h1> Registreer </h1>
-
+<br>
+    <label for"wachtwoord">Wachtwoord</label>
+    <br>
+    <input type="password" placeholder="Voer je nieuwe wachtwoord in" name="wachtwoord" id="wachtwoord" required>
+<br>    
     <label for"naam">Naam</label>
     <br>
     <input type="text" placeholder="Voer je naam in" name="naam" id="naam" required>
 <br>
-    <label for"achteernaam">Achternaam</label>
+    <label for"achternaam">Achternaam</label>
     <br>
     <input type="text" placeholder="Voer je achternaarnaam in" name="achternaam" id="achternaam" required>
 <br>
@@ -28,17 +52,18 @@
     <br>
     <input type="date"  name="gdatum" id="gdatum" required>
 <br>
-    <label for"Adres">Adres</label>
+    <label for"adres">Adres</label>
     <br>
     <input type="text" placeholder="Voer je adres in" name="adres" id="adres" required>
 <br>
-    <label for"Potcode">Postcode</label>
+    <label for"postcode">Postcode</label>
     <br>
     <input type="text" placeholder="Voer je postcode in" name="postcode" id="postcode" required>
 <br>
     <label for"email">E-mail</label>
     <br>
     <input type="email" placeholder="Voer je E-mail in" name="email" id="email" required>
+
 <br>
 <br>
     <button type="submit" name="registeren" id="registreren" value="registreren">registreer</button>    
