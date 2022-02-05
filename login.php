47fd1e933a63238a7761b1851d1d386109981ae8
Sarah Vogelzang <sarah.vogelzang@student.nhlstenden.com>
2022-02-05 16:52:05 +0100

    opzet login pagina

---
+++ login.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+include_once("Config/config.php");
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <form action="<?=htmlspecialchars($_SERVER['PHP_SELF'])?>" method="POST">
+        <label for="login">Login</label>
+        <br>
+        <input type="email" name="email" id="login" placeholder="Voer uw email adres in">
+        <br><br>
+        <label for="password">Wachtwoord</label>
+        <br>
+        <input type="password" id-"password" name="password" placeholder="voer uw wachtwoord in">
+        <br><br>
+        <button type="submit" name="inloggen" id="inloggen" value="inloggen">Inloggen</button>   
+
+    </form>
+    
+</body>
+</html>
