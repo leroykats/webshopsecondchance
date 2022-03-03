310ded7764b8a6428d669d6849f0b94aed851fc7
Sarah Vogelzang <sarah.vogelzang@student.nhlstenden.com>
2022-03-03 13:09:50 +0100

    Admin kan acocunts aanmaklen (de rest)

--- Pages/header.php
+++ Pages/header.php
@@ -11,6 +11,7 @@
     }
     elseif($_SESSION["loggedIn"] && $_SESSION["accountType"] == 0){
         echo '<li><a href="addproduct.php">Add product</a></li>';
+        echo '<li><a href="accounts.php">Beheer accounts</a></li>';
         echo '<li><a href="logout.php">Uitloggen</a></li>';
     }elseif($_SESSION["loggedIn"] && $_SESSION["accountType"] == 1){
         echo '<li><a href="order.php">orders</a></li>';
